d0e51451a37dcc17be38e3789a24038c71e250ef
Nishanth K J <kjwork298@gmail.com>
2026-08-11 09:55:18 +0530

    feat: add column mapping route

--- client/src/app/router.jsx
+++ client/src/app/router.jsx
@@ -1,57 +1,48 @@
- import {
+import {
   Navigate,
   createBrowserRouter,
 } from 'react-router-dom';
 
 import AppShell from '../components/layout/AppShell';
-
+import MappingPage from '../pages/MappingPage';
 import PreviewPage from '../pages/PreviewPage';
 import UploadPage from '../pages/UploadPage';
 
-export const router =
-  createBrowserRouter([
-    {
-      path: '/',
-      element: <AppShell />,
-
-      children: [
-        {
-          index: true,
-
-          element: (
-            <Navigate
-              to="/imports/new"
-              replace
-            />
-          ),
-        },
-
-        {
-          path:
-            'imports/new',
-
-          element:
-            <UploadPage />,
-        },
-
-        {
-          path:
-            'imports/:uploadId/preview',
-
-          element:
-            <PreviewPage />,
-        },
-      ],
-    },
-
-    {
-      path: '*',
-
-      element: (
-        <Navigate
-          to="/imports/new"
-          replace
-        />
-      ),
-    },
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppShell />,
+    children: [
+      {
+        index: true,
+        element: (
+          <Navigate
+            to="/imports/new"
+            replace
+          />
+        ),
+      },
+      {
+        path: 'imports/new',
+        element: <UploadPage />,
+      },
+      {
+        path: 'imports/:uploadId/preview',
+        element: <PreviewPage />,
+      },
+      {
+        path: 'imports/:uploadId/mapping',
+        element: <MappingPage />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: (
+      <Navigate
+        to="/imports/new"
+        replace
+      />
+    ),
+  },
+]);
